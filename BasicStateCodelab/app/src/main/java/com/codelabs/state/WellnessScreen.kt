6de5286d1f66a5278a14697164b4501445c21050
Luiz Fernando <LUIZFERNANDOJUNIOR9999@GMAIL.COM>
2024-11-24 08:43:44 -0300

    Primeiro commit

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessScreen.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessScreen.kt
@@ -18,8 +18,10 @@ package com.codelabs.state
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 
+@Preview
 @Composable
 fun WellnessScreen(
     modifier: Modifier = Modifier,
